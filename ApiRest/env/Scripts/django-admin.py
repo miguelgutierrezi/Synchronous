29578e915ecc29d56534f4b3e1d859dd8d491ea7
Miguel Gutiérrez <miguel_gutierrez@javeriana.edu.co>
2018-09-30 11:34:50 -0500

    ApiRest update

--- ApiRest/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\miguel\documents\github\synchronous\apirest\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- ApiRest/restBackend/quickRest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuickrestConfig(AppConfig):
-    name = 'quickRest'
