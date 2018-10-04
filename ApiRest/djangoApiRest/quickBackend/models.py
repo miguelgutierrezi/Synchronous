cc8c522314766a327128ed05e77a9fb7870e3eb2
elMolda <david_molano@hotmail.com>
2018-10-04 09:43:38 -0500

    Se agrego campo creditos a materia

--- ApiRest/djangoApiRest/quickBackend/models.py
+++ ApiRest/djangoApiRest/quickBackend/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    credits = models.IntegerField(default=1)
 
     def __str__(self):
         return self.name
