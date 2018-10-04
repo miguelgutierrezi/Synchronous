5bd00f7b4dd267bf89a279bef5a666b825c41320
elMolda <david_molano@hotmail.com>
2018-10-04 09:57:26 -0500

    Se agregaron campos a subject

--- ApiRest/djangoApiRest/quickBackend/models.py
+++ ApiRest/djangoApiRest/quickBackend/models.py
@@ -7,6 +7,10 @@ class Subject(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     credits = models.IntegerField(default=1)
+    grade = models.FloatField(default=1)
+    teacher = models.CharField(max_length=100, default=' ')
+    starTime = models.CharField(max_length=5,  default='00:00')
+    endTime = models.CharField(max_length=5, default='00:00')
 
     def __str__(self):
         return self.name
