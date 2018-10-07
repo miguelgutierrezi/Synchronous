ea3c5bab8cf0a2773c46644b818f31c40d9062ca
Miguel Gutiérrez <miguel_gutierrez@javeriana.edu.co>
2018-10-07 10:11:04 -0500

    Avance Branch 1

--- ApiRest/djangoApiRest/djangoApiRest/settings.py
+++ ApiRest/djangoApiRest/djangoApiRest/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'quickBackend',
+
 ]
 
 REST_FRAMEWORK = {
@@ -48,6 +50,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +60,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8100/'
+)
 
 ROOT_URLCONF = 'djangoApiRest.urls'
 
