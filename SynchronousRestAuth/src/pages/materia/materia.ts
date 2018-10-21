db463d9e3733d7d6aa385891a684d3a7bc3a1850
William Baquero <williambaqueror@gmail.com>
2018-10-20 19:54:28 -0500

    formularios

--- SynchronousRestAuth/src/pages/materia/materia.ts
+++ SynchronousRestAuth/src/pages/materia/materia.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../providers';
-
 /**
- * Generated class for the PrincipalPage page.
+ * Generated class for the MateriaPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -11,11 +10,13 @@ import { User } from '../../providers';
 
 @IonicPage()
 @Component({
-  selector: 'page-principal',
-  templateUrl: 'principal.html',
+  selector: 'page-materia',
+  templateUrl: 'materia.html',
 })
-export class PrincipalPage {
+export class MateriaPage {
+
   constructor(public navCtrl: NavController,public user: User, public navParams: NavParams) {
+  
   }
   account: { email: string, password: string } = {
     email: 'test@example.com',
@@ -23,7 +24,10 @@ export class PrincipalPage {
   };
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PrincipalPage');
+    console.log('ionViewDidLoad MateriaPage');
+  }
+  agregar(){
+    this.navCtrl.push('PrincipalPage');
   }
   doLogout() {
     this.user.signup(this.account).subscribe((resp) => {
